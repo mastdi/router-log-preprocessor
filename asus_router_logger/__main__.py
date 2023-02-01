c4114d6608c5d538916af0bda11ee9060fc9e5f8
mastdi <Mastdieu@protonmail.com>
2023-02-01 20:47:35 +0100

    Add initial settings

--- asus_router_logger/__main__.py
+++ asus_router_logger/__main__.py
@@ -11,8 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from asus_router_logger.settings import settings
+
 
 def main() -> None:
     """Main entry point of the Asus Router Logger (ARL)
     """
-    print("Hello world!")
+    print(settings().dict())
---
+++ asus_router_logger/settings.py
@@ -0,0 +1,40 @@
+#  Copyright (c) 2023. Martin Storgaard Dieu <martin@storgaarddieu.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from functools import lru_cache
+from typing import Union
+
+from pydantic import BaseSettings, IPvAnyAddress, Field
+
+
+class Settings(BaseSettings):
+    """Define the settings of the application.
+    """
+    log_server_host: Union[IPvAnyAddress, str] = Field(
+        default="0.0.0.0",
+        description="IP address or host name of the local interface to bind to."
+    )
+    log_server_port: int = Field(default=514, description="Local port to bind to.")
+    log_server_reuse_port: bool = Field(
+        default=False,
+        description="True to allow multiple sockets to bind to the same address/port "
+                    "(not supported on Windows)"
+    )
+
+
+@lru_cache
+def settings() -> Settings:
+    """Get the settings of the applications.
+    :return:
+    """
+    return Settings()
