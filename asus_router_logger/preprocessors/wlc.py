15d089c3f8984af3c09094849e52b30f8653cad8
mastdi <Mastdieu@protonmail.com>
2023-02-17 21:48:50 +0100

    Add WLC preprocessor for DISASSOCIATION events

--- asus_router_logger/preprocessors/wlc.py
+++ asus_router_logger/preprocessors/wlc.py
@@ -14,12 +14,38 @@
 import logging
 
 import asus_router_logger.settings
+from asus_router_logger.domain.mac_base import MAC
+from asus_router_logger.domain.wlc import WlcEvent, WlcEventModel
 from asus_router_logger.log_server.rfc3164_parser import LogRecord
 
 
-async def handle_wireless_lan_controller_event(record: LogRecord) -> None:
+def handle_wireless_lan_controller_event(record: LogRecord) -> WlcEventModel:
     settings = asus_router_logger.settings.settings()
     logger = logging.getLogger(settings.logging_name_base)
     logger.debug("Received WLC event daemon log: %r", record)
 
-    assert record.process == "wlceventd"
+    # Remove message type from the message, i.e. wlceventd_proc_event(xxx):
+    message = record.message[record.message.find(": ") + 2 :]
+    # The parts come in a comma separated list of key:value pairs
+    message_parts = message.split(", ")
+    # First part contains location and event
+    location, event = message_parts[0].split(":", maxsplit=1)
+    # A mac address is 17 characters long and is found as the last part of the event
+    mac_address = event[-17:]
+    # Next part is status
+    assert message_parts[1].startswith("status")
+    status = message_parts[1].split(":")[1]
+    # Next part is reason of the event
+    assert message_parts[2].startswith("reason")
+    reason = message_parts[2].split(":")[1]
+    # Last part is the Received Signal Strength Indicator
+    assert message_parts[3].startswith("rssi")
+    rssi = message_parts[3].split(":")[1]
+
+    return WlcEventModel(
+        mac_address=MAC(mac_address),
+        event=WlcEvent.from_reason(reason),
+        location=location,
+        status=int(status),
+        rssi=int(rssi),
+    )
---
+++ tests/preprocessors/test_wlc.py
@@ -0,0 +1,41 @@
+#  Copyright (c) 2023. Martin Storgaard Dieu <martin@storgaarddieu.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import asus_router_logger.log_server.rfc3164_parser
+from asus_router_logger.domain.mac_base import MAC
+from asus_router_logger.domain.wlc import WlcEvent
+from asus_router_logger.preprocessors.wlc import handle_wireless_lan_controller_event
+
+_RECORD1 = asus_router_logger.log_server.rfc3164_parser.LogRecord(
+    facility=1,
+    severity=5,
+    timestamp=asus_router_logger.log_server.rfc3164_parser.timestamp_to_datetime(
+        "Feb", "2", "13", "02", "51"
+    ),
+    hostname="GT-AX11000-ABCD-1234567-E",
+    process="wlceventd",
+    process_id=None,
+    message="wlceventd_proc_event(511): eth7: Disassoc AB:CD:EF:01:23:45, status: 0, "
+    "reason: Disassociated because sending station is leaving (or has left) "
+    "BSS (8), rssi:0",
+)
+
+
+def test_disassociated_message():
+    model = handle_wireless_lan_controller_event(_RECORD1)
+
+    assert model.location == "eth7"
+    assert model.mac_address == MAC("AB:CD:EF:01:23:45")
+    assert model.status == 0
+    assert model.event == WlcEvent.DISASSOCIATION
+    assert model.rssi == 0
